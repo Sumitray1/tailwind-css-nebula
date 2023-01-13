f1fa2f6702af5360a8734c3768336b73bcf3d54a
Sumitray1 <sumitray206@gmail.com>
2023-01-13 09:25:35 +0545

    nav bar completed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,27 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 function Layout({ children }) {
+  const location = useLocation();
   const menuItems = [
     {
       title: "Home",
       paths: "/",
-      icon: "",
+      icon: "ri-home-smile-line",
     },
     {
       title: "About",
       path: "/abouts",
-      icon: "",
+      icon: "ri-information-line",
     },
     {
       title: "Clients",
       path: "/clients",
-      icon: "",
+      icon: "ri-user-location-fill",
     },
     {
       title: "Contact",
       path: "/contacts",
-      icon: "",
+      icon: "ri-contacts-fill",
     },
   ];
   return (
@@ -29,9 +31,26 @@ function Layout({ children }) {
       {/* Footer */}
       <div className="fixed bottom-3  left-0 right-0">
         <div className="flex w-full justify-center ">
-          {menuItems.map((menu) => (
-            <div>
-              {menu.title} {`${menu.path}`}
+          {menuItems.map((menu, index) => (
+            <div className="flex flex-col justify-end items-center">
+              {location.pathname === menu.path && (
+                <div className="h-10 w-20 bg-primary rounded-t-full  text-center pt-3">
+                  <i className={`${menu.icon} text-white text-xl  `}></i>
+                </div>
+              )}
+              <div
+                className={`px-20 bg-primary py-4   ${
+                  index === 0 && "rounded-l"
+                } ${index === menuItems.length - 1 && "rounded-r"}
+                flex items-center justify-center space-x-2`}
+              >
+                {location.pathname !== menu.path && (
+                  <i className={`${menu.icon} text-secondary text-xl  `}></i>
+                )}
+                <Link to={menu.path} className="text-secondary text-xl">
+                  {menu.title}
+                </Link>
+              </div>
             </div>
           ))}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#112B3c",
+        secondary: "#F66B0E",
+      },
+    },
   },
   plugins: [],
 };
