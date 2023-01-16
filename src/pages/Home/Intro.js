de6647015eb97e9c083d2db93f89d24e12f3d9f7
Sumitray1 <sumitray206@gmail.com>
2023-01-16 18:20:46 +0545

    home page created

--- src/pages/Home/Intro.js
+++ src/pages/Home/Intro.js
@@ -2,21 +2,21 @@ import React from "react";
 
 function Intro() {
   return (
-    <div className="grid grid-cols-2 h-screen items-center px-12">
-      <div className="flex flex-col space-y-10 pl-20">
-        <h1 className=" text-primary text-7xl font-semibold">
+    <div className="grid grid-cols-2 h-screen items-center">
+      <div className="flex flex-col space-y-8 ">
+        <h1 className=" text-primary text-6xl font-semibold">
           The Best <b className="text-secondary">marketing</b>
         </h1>
-        <h1 className=" text-primary text-5xl font-semibold">
+        <h1 className=" text-primary text-4xl font-semibold">
           is telling a story people
         </h1>
 
-        <h1 className="text-secondary text-5xl font-semibold">want to hear</h1>
+        <h1 className="text-secondary text-4xl font-semibold">want to hear</h1>
         <button className="px-28 py-3 bg-secondary text-white max-w-max rounded">
           Get Started
         </button>
       </div>
-      <div className="p-10">
+      <div className="">
         <lottie-player
           src="https://assets7.lottiefiles.com/packages/lf20_01jwptn4.json"
           background="transparent"
