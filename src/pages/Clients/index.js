dfcc0b18375f8327186077e0a0f60458fdb7b2b7
Sumitray1 <sumitray206@gmail.com>
2023-05-23 11:07:03 +0545

    client changed

--- src/pages/Clients/index.js
+++ src/pages/Clients/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Layout from "../../components/Layout";
 import Intro from "./Intro";
-import WhyUs from "./WhyUs";
 function Clients() {
   return (
     <Layout>
       <div>
         <Intro />
-        <WhyUs />
+  
       </div>
     </Layout>
   );
